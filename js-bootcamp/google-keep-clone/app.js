c9f27d4610cfac15db3b2c7430814e318887851d
robinovitch61 <leorobinovitch@gmail.com>
2020-01-15 13:08:24 -0800

    add capability to add notes

--- js-bootcamp/google-keep-clone/app.js
+++ js-bootcamp/google-keep-clone/app.js
@@ -1,8 +1,11 @@
 class App {
   constructor() {
+    this.notes = [];
+
     // '$' indicates an element rather than data
     this.$form = document.querySelector("#form");
     this.$noteTitle = document.querySelector('#note-title');
+    this.$noteText = document.querySelector('#note-text');
     this.$formButtons = document.querySelector('#form-buttons');
 
     this.addEventListeners();
@@ -11,6 +14,18 @@ class App {
   addEventListeners() {
     document.body.addEventListener("click", event => {
       this.handleFormClick(event)
+    });
+
+    this.$form.addEventListener('submit', event => {
+      // prevent default of page reloading
+      event.preventDefault(); 
+      const title = this.$noteTitle.value;
+      const text = this.$noteText.value;
+      const hasNote = title || text;
+
+      if (hasNote) {
+        this.addNote({ title, text });
+      }
     });
   }
 
@@ -34,6 +49,17 @@ class App {
     this.$form.classList.remove('form-open');
     this.$noteTitle.style.display = 'none';
     this.$formButtons.style.display = 'none';
+  }
+
+  addNote(note) {
+    const newNote = {
+      title: note.title,
+      text: note.text,
+      color: "white",
+      id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 0,
+    }
+    this.notes = [...this.notes, newNote];
+    console.log(this.notes);
   }
 }
 
