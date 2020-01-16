edd165fd0fe55114fa75a858cebae720ed654a7f
robinovitch61 <leorobinovitch@gmail.com>
2020-01-15 22:08:15 -0800

    add note if click away from form. Also make close button work

--- js-bootcamp/google-keep-clone/app.js
+++ js-bootcamp/google-keep-clone/app.js
@@ -9,6 +9,7 @@ class App {
     this.$formButtons = document.querySelector("#form-buttons");
     this.$placeholder = document.querySelector("#placeholder");
     this.$notes = document.querySelector("#notes");
+    this.$formCloseButton = document.querySelector("#form-close-button");
 
     this.addEventListeners();
   }
@@ -29,13 +30,25 @@ class App {
         this.addNote({ title, text });
       }
     });
+
+    this.$formCloseButton.addEventListener('click', event => {
+      // stop actions by further event listeners
+      event.stopPropagation();
+      this.closeForm();
+    });
   }
 
   handleFormClick(event) {
     const isFormClicked = this.$form.contains(event.target);
+    
+    const title = this.$noteTitle.value;
+    const text = this.$noteText.value;
+    const hasNote = title || text;
 
     if (isFormClicked) {
       this.openForm();
+    } else if (hasNote) {
+      this.addNote({ title, text });
     } else {
       this.closeForm();
     }
@@ -55,10 +68,10 @@ class App {
     this.$noteText.value = '';
   }
 
-  addNote(note) {
+  addNote({ title, text }) {
     const newNote = {
-      title: note.title,
-      text: note.text,
+      title,
+      text,
       color: "white",
       id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 0,
     }
