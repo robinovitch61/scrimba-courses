186b07f75b439a5905dfa9e7e662e79119ba7927
robinovitch61 <leorobinovitch@gmail.com>
2020-01-27 13:51:17 -0800

    add mock todo items

--- learn-react-for-free/todo-app/src/App.js
+++ learn-react-for-free/todo-app/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ToDoItem from "./components/ToDoItem";
+import todosData from "./todosData";
 
 export default function App() {
+  const todosComponents = todosData.map(todo => {
+    return <ToDoItem key={todo.id} todo={todo} />
+  })
+  
   return (
     <div className="todo-list">
-      <ToDoItem />
-      <ToDoItem />
-      <ToDoItem />
+      {todosComponents}
     </div>
   )
 }
--- learn-react-for-free/todo-app/src/components/ToDoItem.js
+++ learn-react-for-free/todo-app/src/components/ToDoItem.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function ToDo() {
+export default function ToDo(props) {
   return (
     <div className="todo-item">
-      <input type="checkbox" />
-      <p>Testing</p>
+      <input type="checkbox" checked={props.todo.completed}/>
+      <p>{props.todo.text}</p>
     </div>
   )
 }
