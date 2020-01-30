c291a3f591893a26df7a698dc8c98c0a47a9401d
robinovitch61 <leorobinovitch@gmail.com>
2020-01-30 11:51:18 -0800

    add dynamic styling and complete app

--- learn-react-for-free/todo-app/src/components/ToDoItem.js
+++ learn-react-for-free/todo-app/src/components/ToDoItem.js
@@ -2,19 +2,20 @@ import React from "react";
 
 export default class TodoItem extends React.Component {
   render () {
-
     const handleToDoChange = () => {
       this.props.handleChange(this.props.todo.id);
     }
 
-    return (
+    const todoTextClass = this.props.todo.completed ? "completed" : "todo";
+
+    return (      
       <div className="todo-item">
         <input
           type="checkbox"
           checked={this.props.todo.completed}
           onChange={handleToDoChange}
         />
-        <p>{this.props.todo.text}</p>
+        <p className={todoTextClass + "-text"}>{this.props.todo.text}</p>
       </div>
     )
   }
